829a7d6ce05d35b95b9885dcbdf5875a4cd435d9
Kimonides Alexander <kimonide@gmail.com>
2022-08-15 16:55:00 +0100

    Revert "first commit"
    This reverts commit db908048168ccb404e296d89ceabbdcef3e82b24.

---
+++ transaction.rb
@@ -0,0 +1,13 @@
+class Transaction
+    def initialize(date, description, amount, currency, account_name)
+      @date = date
+      @description = description
+      @amount = amount
+      @currency = currency
+      @account_name = account_name
+    end
+
+    def to_json(options = {})
+      JSON.pretty_generate({:date => @date, :description => @description, :amount => @amount, :currency => @currency, :account_name => @account_name}, options)
+    end
+end
